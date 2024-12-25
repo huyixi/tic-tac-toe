c4294a1b3d0b25a86f2dd366675768e7ba0bec98
huyixi <hi@huyixi.com>
2024-12-25 16:55:55 +0800

    chore

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,8 @@ function Board() {
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        console.log("winner", squares[a]);
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
         return squares[a];
-      }
     }
     return null;
   }
